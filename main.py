ca4647734e5286e463931eb10b4e8756b38b71a2
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-09-27 01:02:01 +0900

    playoutモードを実装

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import random
 
 class Hand:
     hand = []
-    def __init__(self,*args):
-        if not len(args) == 1:
+    def __init__(self,initial=[],mode="Normal"):
+        self.mode = mode
+        if not len(initial) >= 3:
             self.draw()
             self.draw()
             self.draw()
         else:
-            self.hand = args[0]
+            self.hand = initial
     def getHand(self):
         return sorted(self.hand)
     def draw(self):
-        if(len(self.hand) <= 3):
+        if(len(self.hand) <= 3 or self.mode is 'playout'):
             num = random.randint(1,9)
             self.hand.append(num)
             return self.hand
@@ -23,6 +24,7 @@ class Hand:
         if(len(self.hand) == 4):
             self.hand.remove(num)
     def check(self):
+        matched_tunk = []
         prev_num = 0
         match_count = 0
         singled_array = list(set(self.hand))
@@ -32,10 +34,14 @@ class Hand:
         for i in range(0,hand_length):
             if(i + 2 < hand_length):
                 if singled_array[i] == singled_array[i + 1] -1 == singled_array[i + 2] -2:
+                    self.matched_tunk = singled_array[i:i+3]
                     result = True
             else:
                 break
         return result
+    def get_cleared_part(self):
+        self.check()
+        return self.matched_tunk
 
 if __name__ == '__main__':
     hand = Hand()
--- test.py
+++ test.py
@@ -7,9 +7,30 @@ class TestStringMethods(unittest.TestCase):
         hand_length = len(self.hand.getHand())
         self.assertEqual(hand_length, 3)
     def test_check_func(self):
-        self.hand = Hand([1,2,7,8,8])
+        self.hand = Hand([1,2,7,8,9])
         result = self.hand.check()
         self.assertEqual(result, True)
+    def test_playout(self):
+        self.hand = Hand(initial=[1,2,7,8,9],mode="playout")
+        result = self.hand.check()
+        self.assertEqual(result, True)
+    def test_playout_can_mulit_length(self):
+        hand = Hand(initial=[1,2,7,8,9],mode="playout")
+        hand.draw()
+        hand.draw()
+        hand.draw()
+        result = hand.check()
+        hand_length = len(hand.getHand())
+    def test_get_cleared_array(self):
+        hand = Hand(initial=[1,2,7,8,9,2,2],mode="playout")
+        result = hand.get_cleared_part()
+        self.assertEqual(len(result),3)
+        self.assertEqual(int(result[0]),int(result[1]) - 1)
+        self.assertEqual(int(result[1]),int(result[2]) - 1)
+        self.assertEqual(int(result[0]),7)
+        self.assertEqual(int(result[1]),8)
+        self.assertEqual(int(result[2]),9)
+
   #
   # def test_isupper(self):
   #     self.assertTrue('FOO'.isupper())
